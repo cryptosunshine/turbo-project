8100f419bf207c45fb977d6afc576e807a2a3ddc
shine <shouhui.sun@gmail.com>
2024-03-12 14:30:17 +0800

    build

---
+++ packages/ui/dist/code.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Code = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
+function Code({ children, className, }) {
+    return (0, jsx_runtime_1.jsx)("code", { className: className, children: children });
+}
+exports.Code = Code;
